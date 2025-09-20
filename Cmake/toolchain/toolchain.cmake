be58c95103495f7755bf1ffc9e00293a63862d97
Fogotcheck <fogotcheck@live.com>
2025-09-20 12:28:06 +0300

    Initial

---
+++ Cmake/toolchain/toolchain.cmake
@@ -0,0 +1,50 @@
+set(CMAKE_SYSTEM_PROCESSOR "riscv32" CACHE STRING "")
+set(CMAKE_SYSTEM_NAME "Generic" CACHE STRING "")
+
+# Specify toolchain. NOTE When building from inside STM32CubeIDE the location of the toolchain is resolved by the "MCU Toolchain" project setting (via PATH).
+set(TOOLCHAIN_PREFIX "riscv-none-elf-")
+
+find_program(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc" HINTS ${TOOLCHAIN_PATH})
+find_program(CMAKE_ASM_COMPILER "${TOOLCHAIN_PREFIX}gcc" HINTS ${TOOLCHAIN_PATH})
+find_program(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++" HINTS ${TOOLCHAIN_PATH})
+
+find_program(CMAKE_AR "${TOOLCHAIN_PREFIX}ar" HINTS ${TOOLCHAIN_PATH})
+find_program(CMAKE_LINKER "${TOOLCHAIN_PREFIX}ld" HINTS ${TOOLCHAIN_PATH})
+find_program(CMAKE_OBJCOPY "${TOOLCHAIN_PREFIX}objcopy" HINTS ${TOOLCHAIN_PATH})
+find_program(CMAKE_RANLIB "${TOOLCHAIN_PREFIX}ranlib" HINTS ${TOOLCHAIN_PATH})
+find_program(CMAKE_STRIP "${TOOLCHAIN_PREFIX}strip" HINTS ${TOOLCHAIN_PATH})
+
+set(CMAKE_ASM_FLAGS "-x assembler-with-cpp")
+
+add_compile_options(
+    -march=rv32imfc_zba_zbb_zbc_zbs_zicsr
+    -mabi=ilp32f
+    -nostartfiles
+    -mstrict-align
+    -mno-save-restore
+    -ffunction-sections
+    -fdata-sections
+    -fno-builtin
+    -Wall
+)
+
+add_link_options(
+    -march=rv32imfc_zba_zbb_zbc_zbs_zicsr
+    -mabi=ilp32f
+    -nostartfiles
+
+    -ffunction-sections
+    -fdata-sections
+    -Wl,--gc-sections
+    
+    # -Wl,--allow-multiple-definition
+    -Wl,-Map=${PROJECT_NAME}.map,--cref,--print-memory-usage
+    -Wl,--start-group
+    -lc
+    -lm
+    -lgcc
+    -Wl,--end-group
+
+    --sysroot=/usr/riscv64-elf
+)
+
---
+++ Lib/freeRTOS/config/FreeRTOSConfig.h
@@ -0,0 +1,107 @@
+#ifndef FREERTOS_CONFIG_H
+#define FREERTOS_CONFIG_H
+
+/*-----------------------------------------------------------
+ * Application specific definitions.
+ *
+ * These definitions should be adjusted for your particular hardware and
+ * application requirements.
+ *
+ * These parameters and more are described within the 'configuration' section of the
+ * FreeRTOS API documentation available on the FreeRTOS.org web site.
+ *
+ * See http://www.freertos.org/a00110.html
+ *----------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+/* Section where include file can be added */
+/* USER CODE END Includes */
+
+/* Ensure definitions are only used by the compiler, and not by the assembler. */
+#if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
+  #include <stdint.h>
+#endif
+
+#include "mtimer.h"
+
+#define configMTIME_BASE_ADDRESS                 RISCV_MTIME_ADDR
+#define configMTIMECMP_BASE_ADDRESS              RISCV_MTIMECMP_ADDR
+#define configISR_STACK_SIZE_WORDS               ( 512 ) 
+
+#define configENABLE_FPU                         0
+#define configENABLE_MPU                         0
+#define portARMV8M_MINOR_VERSION                 0
+
+#define configUSE_PREEMPTION                     1
+#define configSUPPORT_STATIC_ALLOCATION          0
+#define configSUPPORT_DYNAMIC_ALLOCATION         1
+#define configUSE_IDLE_HOOK                      0
+#define configUSE_TICK_HOOK                      0
+#define configCPU_CLOCK_HZ                       16000000UL
+#define configTICK_RATE_HZ                       ((TickType_t)1000)
+#define configMAX_PRIORITIES                     ( 32 )
+#define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
+#define configTOTAL_HEAP_SIZE                    ((size_t)16*1024)
+#define configMAX_TASK_NAME_LEN                  ( 16 )
+#define configUSE_TRACE_FACILITY                 1
+#define configUSE_16_BIT_TICKS                   24
+#define configUSE_MUTEXES                        1
+#define configQUEUE_REGISTRY_SIZE                8
+#define configUSE_RECURSIVE_MUTEXES              1
+#define configUSE_COUNTING_SEMAPHORES            1
+#define configUSE_PORT_OPTIMISED_TASK_SELECTION  1
+
+/* USER CODE BEGIN MESSAGE_BUFFER_LENGTH_TYPE */
+/* Defaults to size_t for backward compatibility, but can be changed
+   if lengths will always be less than the number of bytes in a size_t. */
+#define configMESSAGE_BUFFER_LENGTH_TYPE         size_t
+/* USER CODE END MESSAGE_BUFFER_LENGTH_TYPE */
+
+/* Co-routine definitions. */
+#define configUSE_CO_ROUTINES                    0
+#define configMAX_CO_ROUTINE_PRIORITIES          ( 2 )
+
+/* Software timer definitions. */
+#define configUSE_TIMERS                         1
+#define configTIMER_TASK_PRIORITY                ( 20 )
+#define configTIMER_QUEUE_LENGTH                 10
+#define configTIMER_TASK_STACK_DEPTH             256
+
+/* The following flag must be enabled only when using newlib */
+#define configUSE_NEWLIB_REENTRANT          0
+
+/* Set the following definitions to 1 to include the API function, or zero
+to exclude the API function. */
+#define INCLUDE_vTaskPrioritySet             1
+#define INCLUDE_uxTaskPriorityGet            1
+#define INCLUDE_vTaskDelete                  1
+#define INCLUDE_vTaskCleanUpResources        0
+#define INCLUDE_vTaskSuspend                 1
+#define INCLUDE_vTaskDelayUntil              1
+#define INCLUDE_vTaskDelay                   1
+#define INCLUDE_xTaskGetSchedulerState       1
+#define INCLUDE_xTimerPendFunctionCall       1
+#define INCLUDE_xQueueGetMutexHolder         1
+#define INCLUDE_uxTaskGetStackHighWaterMark  1
+#define INCLUDE_xTaskGetCurrentTaskHandle    1
+#define INCLUDE_eTaskGetState                1
+
+
+/* Normal assert() semantics without relying on the provision of an assert.h
+header file. */
+/* USER CODE BEGIN 1 */
+#define configASSERT( x ) if ((x) == 0) {taskDISABLE_INTERRUPTS(); for( ;; );}
+/* USER CODE END 1 */
+
+
+/* USER CODE BEGIN Defines */
+#if (configGENERATE_RUN_TIME_STATS==1)
+void RTOS_AppConfigureTimerForRuntimeStats(void);
+uint32_t RTOS_AppGetRuntimeCounterValueFromISR(void);
+
+#define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS() RTOS_AppConfigureTimerForRuntimeStats()
+#define portGET_RUN_TIME_COUNTER_VALUE() RTOS_AppGetRuntimeCounterValueFromISR()
+#endif
+/* USER CODE END Defines */
+
+#endif /* FREERTOS_CONFIG_H */
